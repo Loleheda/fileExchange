05b9e324d6f5b3240bc2a4b9df53284c737b9408
Loleheda <loleheda@gmail.com>
2024-03-10 21:09:08 +0300

    create project

---
+++ src/main/java/ru/pinchuk/fileExchange/FileExchangeApplication.java
@@ -0,0 +1,13 @@
+package ru.pinchuk.fileExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExchangeApplication.class, args);
+	}
+
+}
