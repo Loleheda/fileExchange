79cb184ec04a6f63535c7618ef167cff1e20e146
Loleheda <loleheda@gmail.com>
2024-04-04 12:29:15 +0300

    edit data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,7 @@ INSERT INTO roles (name) VALUES ('ADMIN'), ('USER');
 
 INSERT INTO statuses (name) VALUES ('AVAILABLE'), ('LOCKED'), ('SENT');
 
+INSERT INTO users (login, password, role, email) VALUES ('admin', '$2a$10$Pcmagq2tKAeZNJFUeMsRVuObWpW.LggUrwDl0gNzfSlFJvNGEARqy', 1, 'admin@gmail.com')
+
 
 
